6622e0084f70ffd34eee24db86dfcb4ab7ce6d58
usorfaitheloho <usorfaitheloho@gmail.com>
2022-02-23 18:08:40 -0800

    Update js file

--- modules/task_collection.js
+++ modules/task_collection.js
@@ -1,131 +1,123 @@
- class TaskCollection {
-   
+class TaskCollection {
     constructor() {
-       this.addTask = document.querySelector('.add-task');
-       this.domList = document.querySelector('.todo-list');
-       this.collection = [];
+      this.addTask = document.querySelector('.add-task');
+      this.domList = document.querySelector('.todo-list');
+      this.collection = [];
     }
-    initApp = () =>{
-        this.onLoadList();
-        this.updateDom();
+    initApp = () => {
+      this.onLoadList();
+      this.updateDom();
     }
-
-  updateDom = () =>{
-    const ref = this;
-    this.domList.innerHTML ='';
-    this.collection.forEach((element) =>{
+    updateDom = () => {
+      const ref = this;
+      this.domList.innerHTML = '';
+      this.collection.forEach((element) => {
         let completed = '';
-        if (element.completed){
-            completed = 'done';
+        if (element.completed) {
+          completed = 'done';
         }
-
-    ref.domList.innerHTML =`${ref.domList.innerHTML} <li class ="todo-item">
-    <div class="checker"><span class =""><input class = "list-check-${element.index}" type = "checkbox"></span></div>
-    <span class= "${completed} edit" contentEditable = "true"> ${element.description}</span>
-    <i class="fa fa-trash-o float-right delete"></i>
-    </li>`;
-    }, ref);
-
-    this.collection.forEach((element) => {
+        ref.domList.innerHTML = `${ref.domList.innerHTML} <li class="todo-item">
+        <div class="checker"><span class=""><input class="list-check-${element.index}" type="checkbox"></span></div>
+        <span class="${completed} desc" contentEditable="true">${element.description}</span>
+        <i class="fa fa-trash-o float-right delete"></i>
+        </li>`;
+      }, ref);
+      this.collection.forEach((element) => {
         const checkList = document.querySelector(`.list-check-${element.index}`);
         checkList.checked = false;
-        if (element.completed){
-            checkList.checked = true;
+        if (element.completed) {
+          checkList.checked = true;
         }
-    });
-    this.eventDispatcher();
-  }
-  eventDispatcher = ()=> {
+      });
+      this.eventDispatcher();
+    }
+    eventDispatcher = () => {
       this.onclickeventDispatcher();
       this.onsubmiteventDispatcher();
       this.onediteventDispatcher();
-  }
-
-  onclickeventDispatcher = ()=>{
-    const buttons = document.querySelectorAll('.delete');
-    const ref = this;
-    buttons.forEach((button,index) =>{
-        button.addEventListener('click',(event) =>{
-            const eventIdentifier = event.currentTarget;
-            const localArray = [];
-            let count = 1;
-            eventIdentifier.ref.collection.forEach((element,i) =>{
-                if(i !== eventIdentifier.index) {
-                    eventIdentifier.ref.collection[i].index = count;
-                    localArray.push(eventIdentifier.ref.collection[i]);
-                    count +=1;
-                }
-
-            });
-            eventIdentifier.ref.collection = localArray;
-            eventIdentifier.ref.onSaveList();
-            ref.updateDom();
-
+    }
+    onclickeventDispatcher = () => {
+      const buttons = document.querySelectorAll('.delete');
+      const ref = this;
+      buttons.forEach((button, index) => {
+        button.addEventListener('click', (event) => {
+          const eventIdentifier = event.currentTarget;
+          const localArray = [];
+          let count = 1;
+          eventIdentifier.ref.collection.forEach((element, i) => {
+            if (i !== eventIdentifier.index) {
+              eventIdentifier.ref.collection[i].index = count;
+              localArray.push(eventIdentifier.ref.collection[i]);
+              count += 1;
+            }
+          });
+          eventIdentifier.ref.collection = localArray;
+          eventIdentifier.ref.onSaveList();
+          ref.updateDom();
         });
-        button.index = index ;
+        button.index = index;
         button.ref = ref;
-    },ref);
-  }
-  onsubmiteventDispatcher = () =>{
-      this.addTask.addEventListener('keyup', (event) =>{
-        if(event.keyCode !== 13){
-            return;
+      }, ref);
+    }
+    onsubmiteventDispatcher = () => {
+      this.addTask.addEventListener('keyup', (event) => {
+        if (event.keyCode !== 13) {
+          return;
         }
         const input = event.currentTarget.ref.addTask.value;
-        if(input.replace(/\s/g,'').length || input.length <= 0){
-            return;
+        if (!input.replace(/\s/g, '').length || input.length <= 0) {
+          return;
         }
-
-        if(event.keyCode == 13){
-            event.currentTarget.ref.collection.push({
-                index : (event.currentTarget.ref.collection.length + 1) ,
-                description : input,
-                completed : false,
-            });
-            event.currentTarget.ref.onSaveList();
-            event.currentTarget.ref.addTask.value = '';
+  
+        if (event.keyCode === 13) {
+          event.currentTarget.ref.collection.push({
+            index: (event.currentTarget.ref.collection.length + 1), description: input, completed: false,
+          });
+  
+          event.currentTarget.ref.onSaveList();
+          event.currentTarget.ref.addTask.value = '';
         }
         event.currentTarget.ref.updateDom();
         event.preventDefault();
       });
       this.addTask.ref = this;
-  }
-  onediteventDispatcher = () =>{
-      const ref = this;
-      const listEdit = document.querySelectorAll('.edit');
-      listEdit.forEach((edit, index) => {
-          edit.addEventListener('keyup',(event) =>{
-              if(event.keyCode !== 13){
-                  return;
-              }
-              const refobj = event.currentTarget;
-              let input = refobj.value.innerHTML;
-              refobj.value.innerHTML = input.replace('<br>','');
-              input = refobj.value.innerHTML ;
-              if(input.replace(/\s/g,'').length || input.length <= 0){
-                return;
-            }
-            if( refobj.value.innerHTML !== refobj.ref.collection[refobj.index].description){
-                refobj.value.innerHTML = input.replace('<br>','');
-                refobj.ref.collection[refobj.index].description = refobj.value.innerHTML;
-                refobj.ref.onSaveList();
-                refobj.value.blur();           
-            }
-          });
-          edit.index = index ;
-          edit.ref = ref;
-          edit.value = edit;
-      }, ref);
-  }
-  onSaveList = () =>{
-      localStorage.setItem('application_config',JSON.stringify(this.collection));
-  }
-
-  onLoadList = () =>{
-      if(localStorage.getItem('application_config') != null){
-          this.collection = JSON.parse(localStorage.getItem('application_config'));
-      }
-  }
-      
     }
-export default TaskCollection ; 
+    onediteventDispatcher = () => {
+      const ref = this;
+      const listDesc = document.querySelectorAll('.desc');
+      listDesc.forEach((desc, index) => {
+        desc.addEventListener('keyup', (event) => {    
+          if (event.keyCode !== 13) {
+            return;
+          }
+  
+          const refObj = event.currentTarget;
+          let input = refObj.value.innerHTML;
+          refObj.value.innerHTML = input.replace('<br>', '');
+          input = refObj.value.innerHTML;
+          if (!input.replace(/\s/g, '').length || input.length <= 0) {
+            return;
+          }
+          if (refObj.value.innerHTML !== refObj.ref.collection[refObj.index].description) {
+            refObj.value.innerHTML = input.replace('<br>', '');
+            refObj.ref.collection[refObj.index].description = refObj.value.innerHTML;
+            refObj.ref.onSaveList();
+            refObj.value.blur();
+          }
+        });
+        desc.index = index;
+        desc.ref = ref;
+        desc.value = desc;
+      }, ref);
+    }
+    onSaveList = () => {
+      localStorage.setItem('application_config', JSON.stringify(this.collection));
+    }
+    onLoadList = () => {
+      if (localStorage.getItem('application_config') != null) {
+        this.collection = JSON.parse(localStorage.getItem('application_config'));
+      }
+    }
+  }
+  
+  export default TaskCollection;
